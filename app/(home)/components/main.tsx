4c80832fb9c54f74c11a8da5910d522dd07a2442
Sujanchaudhary <jansujerry12@gmail.com>
2025-05-31 19:56:36 +0545

    push

--- app/(home)/components/main.tsx
+++ app/(home)/components/main.tsx
@@ -14,7 +14,6 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Card, CardContent } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
-
 export default function Main() {
   const popularDestinations = [
     {
